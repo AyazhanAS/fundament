f9f06d89d5a588fc13b585d6f90fca6e2c1c0227
Ayazhan <ayazhan.shansharova@mail.ru>
2021-09-25 00:41:01 +0600

    3-th commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles/App.css"
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./UI/select/MySelect";
@@ -8,12 +8,37 @@ import PostFilter from "./UI/select/PostFilter";
 import MyModal from "./UI/modal/MyModal";
 import MyButton from "./UI/buttons/MyButton";
 import { usePosts } from "./hooks/usePost";
+
+import PostService from "./API/postService";
+import Loader from "./UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 function App() {
     const [posts, setPosts] = useState([])
 
     const [filter, setFilter] = useState({sort:"", query:""})
 
     const [visible, setVisible] = useState(false)
+
+    const [totalCount, setTotalCount] = useState(0)
+
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+
+    useEffect(()=>{
+        fetchPosts()
+    }, [])
+
+    const [fetchPosts, isPostLoading, postError] = useFetching(async()=>{
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers["x-total-count"]
+        setTotalPages(getPageCount(totalCount, limit))
+
+    })
+
+    console.log(totalPages);
 
     const createPost = (newPost)=>{
         setPosts([...posts, newPost])
@@ -35,7 +60,10 @@ function App() {
 
     }
 
+    let pagesArray = getPagesArray(totalPages)
+   
 
+    console.log(pagesArray);
   return (
       <div className="App">
           <MyButton style={{marginTop:"30px"}} onClick={openModal}>Add element</MyButton>
@@ -47,7 +75,19 @@ function App() {
          <div>
           <PostFilter filter={filter} setFilter={setFilter}/>
          </div>
-            <PostList remove = {removePost} posts = {sortedAndSearchedPosts} name = "Название поста"/>
+         {
+             postError&&
+             <h1>Прщйзошла ошибка {postError}</h1>
+         }
+         {isPostLoading
+         ?<div style={{display:"flex", justifyContent:"center", marginTop:"50px"}}><Loader/></div>
+         :<PostList remove = {removePost} posts = {sortedAndSearchedPosts} name = "Название поста"/>
+         }
+         {
+             pagesArray.map(p=>
+                <MyButton>{p}</MyButton>)
+         }
+            
         
 
 
